6b7cd280bb21b60fc555404e20e557c8bb10fdd8
DmitrySiC <petrenko.dmitrii@sicom.pro>
2023-04-20 23:17:59 +0300

    errors hidden

--- index.php
+++ index.php
@@ -6,10 +6,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 define("APP_ROOT", __DIR__);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if(session_status() !== PHP_SESSION_ACTIVE)
 {
     session_start();
